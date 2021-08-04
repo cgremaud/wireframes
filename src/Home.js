0ec0ca8276814e6a44bbba2e4823c5b81a6944a0
C. Gremaud <colyn.gremaud@gmail.com>
2021-08-04 14:39:50 -0500

    got card to display in Home

--- src/Home.js
+++ src/Home.js
@@ -2,17 +2,21 @@ import React, { useState, useEffect } from "react";
 import "./Home.css"
 import Paper from '@material-ui/core/Paper';
 import Card from "@material-ui/core/Card";
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 
 export function Home() {
 
     return(
     <Paper className="paper">
         <div className="row home">
-          <div className="col-4" align="center">  
+          <div className="col-12" align="center"> 
+          <CardActionArea>
             <Card className="card">
-                Text Lorem ipsum dolor sit amet
+            <CardContent>Text</CardContent>
             </Card>
-            
+        </CardActionArea> 
         
           </div>
           
