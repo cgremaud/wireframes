9b29321626974c53e5649a638c92e8b38b82ab09
C. Gremaud <colyn.gremaud@gmail.com>
2021-08-04 14:45:02 -0500

    more progress on card display

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 
 export function Home() {
 
@@ -13,8 +14,16 @@ export function Home() {
         <div className="row home">
           <div className="col-12" align="center"> 
           <CardActionArea>
-            <Card className="card">
-            <CardContent>Text</CardContent>
+            <Card variant="outlined">
+            <CardContent>
+                <Typography variant="h3" align="left">Header</Typography>
+                <Typography variant="subtitle1" align="left">
+                    <ul>
+                        <li>List</li>
+                        <li>List</li>
+                    </ul>
+                </Typography>
+            </CardContent>
             </Card>
         </CardActionArea> 
         
