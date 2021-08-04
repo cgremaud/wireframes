c796f9bb48cbcb7ad515b657c0f051d95ef712fc
C. Gremaud <colyn.gremaud@gmail.com>
2021-08-04 11:41:34 -0500

    imported router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="container">
-      
+      <h1>Test</h1>
     </div>
   );
 }
---
+++ src/NavBar.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+export function NavBar() {
+
+}
