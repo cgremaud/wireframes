f5d96b4e813ddaac4f77293d2e7c3a684beaadae
C. Gremaud <colyn.gremaud@gmail.com>
2021-08-04 14:19:39 -0500

    screwing around with cards

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,22 @@
 import React, { useState, useEffect } from "react";
 import "./Home.css"
 import Paper from '@material-ui/core/Paper';
+import Card from "@material-ui/core/Card";
 
 export function Home() {
 
     return(
+    <Paper className="paper">
         <div className="row home">
           <div className="col-4" align="center">  
-            <ul>
-                <li>one</li>
-                <li>two</li>
-                <li>three</li>
-            </ul>
-            <Paper className="card">Text</Paper>
+            <Card className="card">
+                Text Lorem ipsum dolor sit amet
+            </Card>
+            
         
           </div>
           
         </div>
+    </Paper>
     )
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -3,24 +3,27 @@ import {BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 import "./NavBar.css"
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
 
 export function NavBar() {
     return (
-    <nav className="navBar" align="center">
-    <span className="row" align="center">
-        <span className="col-3" align="left"><Button variant="contained" color="primary" className="btn btn-info "><Link to="/">Home</Link></Button> </span>
-        <span className="col-1" align="center">
-            <Button className="btn btn-info" variant="contained" color="primary"><Link to="/my-haves">My Haves</Link> </Button>
+    <Paper>    
+        <nav className="navBar" align="center">
+        <span className="row" align="center">
+            <span className="col-3" align="left"><Button variant="contained" color="primary" className="btn btn-info "><Link to="/">Home</Link></Button> </span>
+            <span className="col-2" align="center">
+                <Button className="btn btn-info" variant="contained" color="primary"><Link to="/my-haves">My Haves</Link> </Button>
+            </span>
+            <span className="col-2">
+                <Button  variant="contained" color="primary" className="btn btn-info"><Link to="/my-needs">My Needs</Link></Button>   
+            </span>
+            <span className="col-2" align="right">
+                <Button className="btn btn-info" variant="contained" color="primary"><Link to="search">Search</Link> </Button>
+            </span>
+        
+            <span className="col-3" align="right"><Button variant="contained" color="primary" className="btn btn-info"><Link to="/profile">Profile</Link></Button></span>
         </span>
-        <span className="col-1">
-            <Button  variant="contained" color="primary" className="btn btn-info"><Link to="/my-needs">My Needs</Link></Button>   
-        </span>
-        <span className="col-1" align="right">
-            <Button className="btn btn-info" variant="contained" color="primary"><Link to="search">Search</Link> </Button>
-        </span>
-      
-        <span className="col-3" align="right"><Button variant="contained" color="primary" className="btn btn-info"><Link to="/profile">Profile</Link></Button></span>
-    </span>
-    </nav>
+        </nav>
+    </Paper>
     )
 }
